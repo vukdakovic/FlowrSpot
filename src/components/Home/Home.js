cd251ab248e532ab0e05795962d6b5404d249bba
Vuk Dakovic <vuk.dakovic1992@gmail.com>
2020-09-21 16:20:39 +0200

    Commiting style changes and added content

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,14 +5,10 @@ import image from "./pl-hero.jpg";
 
 function Home() {
   return (
-    <div>
-      <img src={image} alt="Flower Picture"/>
-      <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text
-        ever since the 1500s, when an unknown printer took a galley of
-        type and scrambled it to make a type specimen book.
-      </p>
+    <div className="div-image">
+      <h1>Discover flowers around you</h1>
+      <h4>Explore between more than 8.427 sightings</h4>
+      <input/>
     </div>
   );
 }
