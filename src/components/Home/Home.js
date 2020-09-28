91b203fece95d503aea6ed93d575fcd23324bbee
Vuk Dakovic <vuk.dakovic1992@gmail.com>
2020-09-28 23:31:23 +0200

    commit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,10 +5,22 @@ import image from "./pl-hero.jpg";
 
 function Home() {
   return (
-    <div className="div-image">
-      <h1>Discover flowers around you</h1>
-      <h4>Explore between more than 8.427 sightings</h4>
-      <input/>
+    <div>
+      <div className="div-image">
+        <h1>Discover flowers around you</h1>
+        <h4>Explore between more than 8.427 sightings</h4>
+        <input/>
+      </div>
+      <div>
+        <div className="div-cell"></div>
+        <div className="div-cell"></div>
+        <div className="div-cell"></div>
+        <div className="div-cell"></div>
+        <div className="div-cell"></div>
+        <div className="div-cell"></div>
+        <div className="div-cell"></div>
+        <div className="div-cell"></div>
+      </div>
     </div>
   );
 }
